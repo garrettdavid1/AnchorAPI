7e21b02a5f4b3241e439130b88a4fc1da6f981af
David Garrett <garrettdavid1@gmail.com>
2018-01-21 22:01:20 -0500

    Set up repository, added some endpoints for initializing the first month, adding transactions, and getting a data for a selected month

--- src/database.js
+++ src/database.js
@@ -1,83 +1,183 @@
-var database = function () {
-    var self = this;
-    self.getData = function (monthNum) {
-        var data = [
-            {
-                monthName: 'Jan',
-                monthNum: 0,
-                year: 2018,
-                startingBal: 1200.39,
-                transactions: [
-                    {
-                        transId: 0,
-                        transName: 'Walmart',
-                        transAmount: '15.90',
-                        transType: 'debit',
-                        transDate: new Date('1/5/18')
-                    },
-                    {
-                        transId: 1,
-                        transName: 'Kroger',
-                        transAmount: '1.34',
-                        transType: 'debit',
-                        transDate: new Date('1/8/18'
-                        )
-                    },
-                    {
-                        transId: 2,
-                        transName: 'Paycheckblahblahblah',
-                        transAmount: '200',
-                        transType: 'credit',
-                        transDate: new Date('1/15/18')
-                    },
-                    {
-                        transId: 3,
-                        transName: 'Coyote\'s',
-                        transAmount: '75.23',
-                        transType: 'debit',
-                        transDate: new Date('1/15/18')
-                    },
-                ]
-            },
-            {
-                monthName: 'Feb',
-                monthNum: 1,
-                year: 2018,
-                startingBal: 1200.39,
-                transactions: [
-                    {
-                        transId: 0,
-                        transName: 'Strippers',
-                        transAmount: '315.90',
-                        transType: 'debit',
-                        transDate: new Date('2/4/18')
-                    },
-                    {
-                        transId: 1,
-                        transName: 'Drugs',
-                        transAmount: '50.00',
-                        transType: 'debit',
-                        transDate: new Date('2/11/18')
-                    },
-                    {
-                        transId: 2,
-                        transName: 'Alcohol',
-                        transAmount: '27.39',
-                        transType: 'debit',
-                        transDate: new Date('2/13/18')
-                    },
-                    {
-                        transId: 3,
-                        transName: 'Munchies',
-                        transAmount: '19.89',
-                        transType: 'debit',
-                        transDate: new Date('2/15/18')
-                    },
-                ]
+var database = (function () {
+    var Database = function () {
+        var self = this;
+        self.db;
+        this.connect = function(dbName) {
+            const MongoClient = require('mongodb').MongoClient;
+            const assert = require('assert');
+
+            // Connection URL
+            const url = 'mongodb://localhost:27017';
+
+            // Use connect method to connect to the server
+            MongoClient.connect(url, function (err, client) {
+                assert.equal(null, err);
+                console.log("Connected successfully to server");
+
+                self.db = client.db(dbName);
+            });
+        }
+
+        this.add = function(collectionName, data, callback){
+            if(!Array.isArray(data)){
+                let array = [];
+                array.push(data);
+                data = array;
             }
-        ]
-        return data[parseInt(monthNum)];
+
+            let collection = self.db.collection(collectionName);
+            collection.insertMany(data, function(err, result){
+                if(callback !== undefined) callback(result);
+            });
+
+        };
+
+        this.get = function(collectionName, queryObj, callback){
+            let collection = self.db.collection(collectionName);
+            collection.find(queryObj).toArray(function(err, result){
+                if(callback !== undefined) callback(result);
+            });
+        }
+
+        this.getAll = function(collectionName, callback){
+            let collection = self.db.collection(collectionName);
+            collection.find({}).toArray(function(err, result){
+                if(callback !== undefined) callback(result);
+            });
+        }
+
+        this.update = function(collectionName, queryObj, updateObj, callback){
+            let collection = self.db.collection(collectionName);
+            collection.updateOne(queryObj, { $set: updateObj}, function(err, result){
+                if(callback !== undefined) callback(result);
+            });
+        }
+
+        this.delete = function(collectionName, queryObj, callback){
+            let collection = self.db.collection(collectionName);
+            collection.deleteOne(queryObj, function(err, result){
+                if(callback !== undefined) callback(result);
+            })
+        }
+
+        this.deleteMany = function(collectionName, queryObj, callback){
+            let collection = self.db.collection(collectionName);
+            collection.deleteMany(queryObj, function(err, result){
+                if(callback !== undefined) callback(result);
+            })
+        }
+    };
+    var dbInstance;
+
+    return {
+        connect: function (dbName) {
+            dbInstance = new Database();
+            dbInstance.connect(dbName);
+        },
+        add: function(collection, data, callback){
+            dbInstance.add(collection, data, callback);
+        },
+        get: function(collection, queryObj, callback){
+            dbInstance.get(collection, queryObj, callback);
+        }, 
+        getAll: function(collectionName, callback){
+            dbInstance.getAll(collectionName, callback);
+        },
+        update: function(collection, queryObj, updateObj, callback){
+            dbInstance.update(collection, queryObj, updateObj, callback);
+        },
+        delete: function(collectionName, queryObj, callback){
+            dbInstance.delete(collectionName, queryObj, callback);
+        },
+        deleteMany: function(collectionName, queryObj, callback){
+            dbInstance.deleteMany(collectionName, queryObj, callback);
+        }
     }
-}
+})();
 
 module.exports = database;
+
+
+// var database = function () {
+//     var self = this;
+//     self.data = [
+//         {
+//             monthName: 'Jan',
+//             monthNum: 0,
+//             year: 2018,
+//             startingBal: 1200.39,
+//             transactions: [
+//                 {
+//                     transId: 0,
+//                     transName: 'Walmart',
+//                     transAmount: '15.90',
+//                     transType: 'debit',
+//                     transDate: new Date('1/5/18')
+//                 },
+//                 {
+//                     transId: 1,
+//                     transName: 'Kroger',
+//                     transAmount: '1.34',
+//                     transType: 'debit',
+//                     transDate: new Date('1/8/18'
+//                     )
+//                 },
+//                 {
+//                     transId: 2,
+//                     transName: 'Paycheckblahblahblah',
+//                     transAmount: '200',
+//                     transType: 'credit',
+//                     transDate: new Date('1/15/18')
+//                 },
+//                 {
+//                     transId: 3,
+//                     transName: 'Coyote\'s',
+//                     transAmount: '75.23',
+//                     transType: 'debit',
+//                     transDate: new Date('1/15/18')
+//                 },
+//             ]
+//         },
+//         {
+//             monthName: 'Feb',
+//             monthNum: 1,
+//             year: 2018,
+//             startingBal: 1200.39,
+//             transactions: [
+//                 {
+//                     transId: 0,
+//                     transName: 'Strippers',
+//                     transAmount: '315.90',
+//                     transType: 'debit',
+//                     transDate: new Date('2/4/18')
+//                 },
+//                 {
+//                     transId: 1,
+//                     transName: 'Drugs',
+//                     transAmount: '50.00',
+//                     transType: 'debit',
+//                     transDate: new Date('2/11/18')
+//                 },
+//                 {
+//                     transId: 2,
+//                     transName: 'Alcohol',
+//                     transAmount: '27.39',
+//                     transType: 'debit',
+//                     transDate: new Date('2/13/18')
+//                 },
+//                 {
+//                     transId: 3,
+//                     transName: 'Munchies',
+//                     transAmount: '19.89',
+//                     transType: 'debit',
+//                     transDate: new Date('2/15/18')
+//                 },
+//             ]
+//         }
+//     ]
+//     self.getData = function (monthNum) {
+//         return self.data[parseInt(monthNum)];
+//     }
+// }
+
+// module.exports = database;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 // var data = require('../tempDev/transactionData');
-var Database = require('./database.js')
-var db = new Database();
+var db = require('./database.js');
+var models = require('./models.js');
+models.init();
+var bodyParser = require('body-parser');
+db.connect('Anchor');
 var app = express();
 
 app.use(function(req, res, next) {
@@ -9,15 +12,107 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+/*:::::::::::::::: Routes ::::::::::::::::*/
 app.get('/', function(req, res, next){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/getData/:month', function(req, res, next){
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(db.getData(req.params.month)));
+app.get('/getMonthData/:month/:year', function(req, res, next){
+    var monthNum = parseInt(req.params.month);
+    var year = parseInt(req.params.year);
+    db.get('Month', {'monthNum': monthNum, 'year': year}, function(result){
+        var month = result;
+            var wasNull = month[0] === undefined ? true: false;
+            if(month[0] === undefined){
+                db.getAll('Month', function (result) {
+                    var today = new Date();
+                    month = models.month(monthNum, year)
+
+                    if (monthNum !== today.getMonth() || year !== today.getFullYear()) {
+                        var latestMonth = result[result.length - 1];
+                        month.startingBal = latestMonth.endingBal;
+                    }
+
+                    month.wasNull = wasNull;
+
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify(month));
+                })
+            }else{
+                month = month[0];
+                month.wasNull = wasNull;
+
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(month));
+            }
+    });
 });
+
+app.get('/initStartingBal/:val', function(req, res, next){
+    var today = new Date();
+    var month = {
+        'monthName': models.getMonthName(today.getMonth()),
+        'monthNum': today.getMonth(),
+        'year': today.getFullYear(),
+        'startingBal': req.params.val,
+        'endingBal': req.params.val,
+        'transactions': [],
+        'wasNull': false,
+        'isFirstAvailableMonth': true
+    };
+    db.add('Month', month);
+    res.send(JSON.stringify(month));
+});
+
+app.post('/saveTransaction', function(req, res, next){
+    var params = req.body;
+    var transaction = params.transaction;
+    var date = new Date(transaction.transDate);
+    db.get('Month', {'monthNum': date.getMonth(), 'year': date.getFullYear()}, function(result){
+        var month = result[0];
+        month.transactions = month.transactions === undefined ? [] : month.transactions;
+        if(transaction.id !== undefined){
+            db.update('Transaction', {'_id' : transaction.id}, transaction, function(result){
+                updateMonthWithTransAndSend(month, transaction, date, res);
+            });
+        }else{
+            db.add('Transaction', transaction, function(result){
+                updateMonthWithTransAndSend(month, transaction, date, res);
+            });
+        }
+        
+    })
+})
+/*:::::::::::::::: End of Routes ::::::::::::::::*/
+
+/*:::::::::::::::: Month Helper Functions ::::::::::::::::*/
+function updateMonthWithTransAndSend(month, transaction, date, res){
+    if(month.transactions.length > 0){
+        var thisDate;
+        var inserted = false;
+        transaction.transDate = new Date(transaction.transDate);
+        for(var i = 0; i < month.transactions.length; i++){
+            thisDate = new Date(month.transactions[i].transDate);
+            if(thisDate.getTime() > transaction.transDate.getTime()){
+                month.transactions.splice(i, 0, transaction);
+                inserted = true;
+                break;
+            }
+        }
+        if(!inserted){
+            month.transactions.push(transaction);
+        }
+    }else{
+        month.transactions.push(transaction);
+    }
+    db.update('Month', {'monthNum': date.getMonth(), 'year': date.getFullYear()}, {'transactions': month.transactions, 'endingBal': parseFloat(month.endingBal) + parseFloat(transaction.transAmount)}, function(result){
+        res.send(JSON.stringify(month));
+    });
+}
+/*:::::::::::::::: End of Month Helper Functions ::::::::::::::::*/
 
 var server = app.listen(1337, function(){
     var host = server.address().address;
